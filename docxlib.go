fa9dcdc82a6b7506609bae95d6ec94c94b404fbf
Gonzalo Fernandez-Victorio <gonzalo.fernandez-victorio@basementcrowd.com>
2021-04-30 14:42:00 +0100

    Improve doc on parse

--- docxlib.go
+++ docxlib.go
@@ -22,6 +22,32 @@ func New() *DocxLib {
 }
 
 // Parse generates a new docx file in memory from a reader
+// You can it invoke from a file
+//		readFile, err := os.Open(FILE_PATH)
+//		if err != nil {
+//			panic(err)
+//		}
+//		fileinfo, err := readFile.Stat()
+//		if err != nil {
+//			panic(err)
+//		}
+//		size := fileinfo.Size()
+//		doc, err := docxlib.Parse(readFile, int64(size))
+// but also you can invoke from a webform (BEWARE of trusting users data!!!)
+//
+//	func uploadFile(w http.ResponseWriter, r *http.Request) {
+//		r.ParseMultipartForm(10 << 20)
+//
+//		file, handler, err := r.FormFile("file")
+//		if err != nil {
+//			fmt.Println("Error Retrieving the File")
+//			fmt.Println(err)
+//			http.Error(w, err.Error(), http.StatusBadRequest)
+//			return
+//		}
+//		defer file.Close()
+//		docxlib.Parse(file, handler.Size)
+//	}
 func Parse(reader io.ReaderAt, size int64) (doc *DocxLib, err error) {
 	zipReader, err := zip.NewReader(reader, size)
 	if err != nil {
