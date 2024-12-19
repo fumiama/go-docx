19779b70d881f22e763d40ecf2b3576476136174
guoqiang <guoqiang@antiy.cn>
2024-12-19 16:18:07 +0800

    fix: 修复main函数

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -30,7 +30,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/fumiama/go-docx"
+	"github.com/l0g1n/go-docx"
 )
 
 func main() {
@@ -66,11 +66,11 @@ func main() {
 		para1.AddText("italic").Italic().AddTab()
 		para1.AddText("underline").Underline("double").AddTab()
 		para1.AddText("highlight").Highlight("yellow").AddTab()
-		para1.AddText("font").Font("Consolas", "", "cs").AddTab()
+		para1.AddText("font").Font("Consolas", "", "", "cs").AddTab()
 
 		para2 := w.AddParagraph().Justification("end")
 		para2.AddText("test all font attrs").
-			Size("44").Color("ff0000").Font("Consolas", "", "cs").
+			Size("44").Color("ff0000").Font("Consolas", "", "", "cs").
 			Shade("clear", "auto", "E7E6E6").
 			Bold().Italic().Underline("wave").
 			Highlight("yellow")
